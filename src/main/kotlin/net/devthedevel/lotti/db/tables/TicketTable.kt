3b2ff2d169e67c29202e3432d7df008dbc9e2237
Devin <devthedevel@gmail.com>
2018-07-06 10:06:33 -0230

    Renamed LotteryTicketTable to TicketTable

---
+++ src/main/kotlin/net/devthedevel/lotti/db/tables/TicketTable.kt
@@ -0,0 +1,11 @@
+package net.devthedevel.lotti.db.tables
+
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+
+object TicketTable : Table() {
+    val lottoIndex = integer("lotto_index").references(LotteryTable.id, ReferenceOption.CASCADE).primaryKey()
+    val userId = long("user_id").primaryKey()
+    val approved = integer("approved")
+    val requested = integer("requested")
+}
