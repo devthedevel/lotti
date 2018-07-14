7cffca193a6756f75552ee768f317fcc8404c6d1
Devin <devthedevel@gmail.com>
2018-07-14 10:14:25 -0230

    Added /feedback to /help

--- src/main/kotlin/net/devthedevel/lotti/commands/HelpCommand.kt
+++ src/main/kotlin/net/devthedevel/lotti/commands/HelpCommand.kt
@@ -14,10 +14,11 @@ class HelpCommand(context: CommandContext): Command(context) {
             withContent("Heya ${context.sender.mention(true)}\n")
             appendContent("Here's a list of commands:\n\n")
             appendContent("**/lotti new**    *Creates a new lottery for the server*\n")
-            appendContent("**/lotti buy numTickets**    *Buy numTickets for the current lottery. numTickets must be positive*\n")
+            appendContent("**/lotti buy [user] [numTickets]**    *Buy numTickets for the specified user. Leave user blank to purchase tickets for yourself. numTickets must be positive*\n")
             appendContent("**/lotti draw**    *Draws a winner for the current lottery. The lottery will then close itself*\n")
             appendContent("**/lotti help**    *Displays this helpful help menu*\n")
             appendContent("**/lotti status**  *Displays the current lottery's status*\n")
+            appendContent("**/lotti feedback**  *Send feedback*\n")
 
             if (context.isAdmin) {
                 appendContent("\n")
